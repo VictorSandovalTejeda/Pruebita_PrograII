d2c797a8bad12a2bb478da61728ec8a43864c4fc
pc HP <pc HP@DESKTOP-O81V35S>
2018-03-01 22:09:48 -0600

    Funciones para la prueba Terminadas.

--- src/pruebita/Pruebita.java
+++ src/pruebita/Pruebita.java
@@ -21,6 +21,19 @@ public class Pruebita {
     public static String completarMes (String mes){
         String nuevoMes = null;
         
+        if (mes.equalsIgnoreCase("Ene")) nuevoMes = "Enero";
+        else if (mes.equalsIgnoreCase("Feb")) nuevoMes = "Febrero";
+        else if (mes.equalsIgnoreCase("Mar")) nuevoMes = "Marzo";
+        else if (mes.equalsIgnoreCase("Abr")) nuevoMes = "Abril";
+        else if (mes.equalsIgnoreCase("May")) nuevoMes = "Mayo";
+        else if (mes.equalsIgnoreCase("Jun")) nuevoMes = "Junio";
+        else if (mes.equalsIgnoreCase("Jul")) nuevoMes = "Julio";
+        else if (mes.equalsIgnoreCase("Ago")) nuevoMes = "Agosto";
+        else if (mes.equalsIgnoreCase("Sep")) nuevoMes = "Septiembre";
+        else if (mes.equalsIgnoreCase("Oct")) nuevoMes = "Octubre";
+        else if (mes.equalsIgnoreCase("Nov")) nuevoMes = "Noviembre";
+        else if (mes.equalsIgnoreCase("Dic")) nuevoMes = "Diciembre";
+        
         return nuevoMes;
     }
     
@@ -36,36 +49,59 @@ public class Pruebita {
         }
     }
     
-    //****-***-**
-    //012345678910
     public static String formatearFecha (String fecha) {
         GregorianCalendar calendar = new GregorianCalendar();
         int year = Integer.parseInt(fecha.subSequence(0, 4).toString());
         String mes = fecha.subSequence(5, 8).toString();
         int dia = Integer.parseInt(fecha.subSequence(9, 11).toString());
         
-        if (mes.equalsIgnoreCase("Ene")) {
-            if (dia <=0 || dia > 31) {
+        if (mes.equalsIgnoreCase("Ene") || mes.equalsIgnoreCase("Mar") || mes.equalsIgnoreCase("May") 
+                || mes.equalsIgnoreCase("Jul") || mes.equalsIgnoreCase("Ago") || mes.equalsIgnoreCase("Oct")
+                || mes.equalsIgnoreCase("Dic")) {
+            if (dia < 1 || dia > 31) {
                 return "No hay " + dia + " dias en el mes de " + completarMes(mes);
             }
         }else if (mes.equalsIgnoreCase("Feb")) {
             if (dia == 29) {
                 if (calendar.isLeapYear(year) == false) {
-                    return year + "no es bisiesto";
+                    return "El año " + year + " NO es bisiesto";
                 }
-                
+            }else if (dia > 29) {
+                return "No hay " + dia + " dias en el mes de " + completarMes(mes);
+            }
+        }else if (mes.equalsIgnoreCase("Abr") || mes.equalsIgnoreCase("Jun") || mes.equalsIgnoreCase("Sep")
+                || mes.equalsIgnoreCase("Nov")) {
+            if (dia < 1 || dia > 30) {
+                return "No hay " + dia + " dias en el mes de " + completarMes(mes);
             }
         }
-        
-        
-        
         return year + "-" + completarMes(mes) + "-" + dia;
     }
     
     
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc = new Scanner(System.in);
+        
+        //Funcion esNacional
+        System.out.println("FUNCION ES NACIONAL");
+        System.out.println(esNacional('V'));
+        System.out.println(esNacional('a'));
+        System.out.println(esNacional('A'));
+        System.out.println(esNacional('#'));
+        System.out.println(esNacional('X'));
+        
+        //Funcion formatearFecha
+        System.out.println();
+        System.out.println("FUNCION FORMATEAR FECHA");
+        System.out.println(formatearFecha("2018-Mar-01"));
+        System.out.println(formatearFecha("2019-Feb-29"));
+        System.out.println(formatearFecha("2020-Feb-29"));
+        System.out.println(formatearFecha("1993-Dic-50"));
+        System.out.println(formatearFecha("1995-Dic-31"));
+        
+        
+        
+        /*Scanner sc = new Scanner(System.in);
         int option = 0;
         char origen;
         String fecha;
@@ -95,7 +131,7 @@ public class Pruebita {
                     }
                     break;
             }
-        }while (option != 2);
+        }while (option != 2);*/
         
     }
     
